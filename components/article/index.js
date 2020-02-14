925c9aa0039b87707f461af31e8749857c14d042
unknown <1329298050@qq.com>
2020-02-14 23:21:52 +0800

    feat:首页样式基本完成

---
+++ components/article/index.js
@@ -0,0 +1,72 @@
+// components/article/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    model:{
+      nickName:"时间难倒回",
+      last:"2天",
+      title:"字节跳动飞书实习",
+      desc:"以上就是本文的全部内容，希望本文的内容对大家的学习工作能带来一定的帮助，同时也希望多多支持脚本之家哈哈哈",
+
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
